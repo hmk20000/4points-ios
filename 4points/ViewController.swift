2809d82811752d4080cb041265b48b81c359f4a0
hmk20000 <hmk20000@nate.com>
2016-08-08 14:49:51 +0900

    한글

--- 4points/ViewController.swift
+++ 4points/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //dd
         // Do any additional setup after loading the view, typically from a nib.
     }
 
