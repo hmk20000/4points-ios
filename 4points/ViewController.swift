07cb4aab445554062901bd0dada28456bfe1ed0e
hmk20000 <hmk20000@nate.com>
2016-08-08 14:34:51 +0900

    Initial Commit

---
+++ 4points/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  4points
+//
+//  Created by MediaSTINT on 2016. 8. 8..
+//  Copyright © 2016년 C4TK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
